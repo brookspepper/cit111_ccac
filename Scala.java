d30aaad46f31a4ce7783497fe4817578b5b467db
brookspepper <79780699+brookspepper@users.noreply.github.com>
2021-04-06 22:04:46 -0400

    Final Final Order Tabulator
    The real actual 100% final version of the order tabulator. Including warnings and extra features.

--- Scala.java
+++ Scala.java
@@ -13,14 +13,18 @@ public class Scala
 {
     private static int pizzaSlidersOrdered=0;
     private static int spaghettiOrdered=0;
+    private static int pizzaOrdered=0;
     private static double totalGrossRevenue=0;
     private static double totalMeat=60;
     private static double totalBuns=10;
     private static double pastaServings=10;
     private static double pastaSauce=10;
+    private static int cheese=10;
+    private static int dough=10;
     
     final static double pizzaSlidersPrice=2.95;
     final static double spaghettiPrice=8.50;
+    final static double pizzaPrice=10.95;
             
     public static void main(String[] args)
     {
@@ -29,7 +33,7 @@ public class Scala
         while(tempCont==0)
         {
         Scanner userInput=new Scanner(System.in);
-        System.out.println("Would you like: \n 1. Sliders \n 2. Spaghetti");
+        System.out.println("Would you like: \n 1. Sliders \n 2. Spaghetti \n 3. Pizza");
         String selection=userInput.next();      
         switch(selection)
         {
@@ -43,7 +47,14 @@ public class Scala
                 int numSpaghetti=userInput.nextInt();
                 spaghetti(numSpaghetti);
                 break;
-                 
+            case "3":
+                System.out.println("How many pizza's would you like?");
+                int pizzaNumber=userInput.nextInt();
+                pizzaOrder(pizzaNumber);
+                break;
+            default:
+                System.out.println("Please try again.");
+                break;                 
         }
         System.out.println("Would you like to order again? \n 1. Yes \n 2. No");
         int userCont=userInput.nextInt();
@@ -56,6 +67,7 @@ public class Scala
                 tempCont++;
                 break;                            
         }
+        product();
         }
         statisticResturante();
         System.out.println("Thank you for coming to Tony's Pizza Parlor!");
@@ -74,7 +86,27 @@ public class Scala
         totalBuns=totalBuns-(sliderOrderNumber);
         }
         else
+        {
             System.out.println("We don't have enough product for that!");
+            totalMeat=totalMeat+100;
+            totalBuns=totalBuns+100;
+            System.out.println("Ordering more product...");
+            System.out.println("Done!");
+        }
+        if(totalMeat<=18)
+        {           
+            System.out.println("We're low on meat!");
+            System.out.println("Ordering more...");
+            totalMeat=totalMeat+42;
+            System.out.println("Meat Restocked!");
+        }
+        if(totalBuns<=3)
+        {
+            System.out.println("We're low on buns!");
+            System.out.println("Ordering more...");
+            totalBuns=totalBuns+7;
+            System.out.println("Buns restocked!");
+        }
     }
     
     public static void spaghetti(int spaghettiOrderNumber)
@@ -87,10 +119,77 @@ public class Scala
         System.out.println("That will be "+cost+" $");
         totalGrossRevenue=cost+totalGrossRevenue;
         pastaServings=pastaServings-(spaghettiOrderNumber);
-        pastaSauce=pastaSauce-pastaServings;
+        pastaSauce=pastaSauce-spaghettiOrderNumber;
         }
         else
+        {
             System.out.println("We don't have enough product for that!");
+            pastaServings=pastaServings+100;
+            pastaSauce=pastaSauce+100;
+            System.out.println("Ordering more product...");
+            System.out.println("Done!");
+        }
+        if(pastaServings<=3)
+        {
+            System.out.println("We're low on spaghetti!");
+            System.out.println("Ordering more...");
+            pastaServings=pastaServings+7;
+            System.out.println("Pasta restocked!");
+        }
+        if(pastaSauce<=3)
+        {
+            System.out.println("We're low on sauce!");
+            System.out.println("Ordering more...");
+            pastaSauce=pastaSauce+7;
+            System.out.println("Sauce restocked!");
+        }
+    }
+    public static void pizzaOrder(int pizzaTime)
+    {
+        if(pastaSauce>=pizzaTime && dough>=pizzaTime && cheese>=pizzaTime)
+        {
+            System.out.println(pizzaTime+" Pizza's coming right up!");
+            pizzaOrdered=pizzaOrdered+pizzaTime;
+            double cost=pizzaTime*pizzaPrice;
+            totalGrossRevenue=cost+totalGrossRevenue;
+            cheese=cheese-pizzaTime;
+            pastaSauce=pastaSauce-pizzaTime;
+            dough=dough-pizzaTime;
+            System.out.println("That will be "+cost+" $");
+        }        
+        else
+        {    
+            System.out.println("We don't have enough product for that!");
+            dough=dough+100;
+            pastaSauce=pastaSauce+100;
+            cheese=cheese+100;
+            System.out.println("Ordering more product...");
+            System.out.println("Done!");
+        }      
+        
+        if(cheese<=3)
+        {
+            System.out.println("We're low in cheese!");
+            System.out.println("Ordering more...");
+            System.out.println("Cheese restocked!");
+            cheese=cheese+7;
+        }
+        
+        if(dough<=3)
+        {
+            System.out.println("We're running out of dough!");
+            System.out.println("Making more...");            
+            dough=dough+7;
+            System.out.println("Dough made!");
+        }
+        
+        if(pastaSauce<=3)
+        {
+            System.out.println("We're low on sauce!");
+            System.out.println("Ordering more...");
+            pastaSauce=pastaSauce+7;
+            System.out.println("Sauce restocked!");
+        }
     }
     
     public static void statisticResturante()
@@ -98,11 +197,17 @@ public class Scala
         System.out.println("*****Restaurant Statistics*****");
         System.out.println(pizzaSlidersOrdered+" Pizza sliders have been ordered.");
         System.out.println(spaghettiOrdered+" Servings of spaghetti have been ordered.");
+        System.out.println(pizzaOrdered+" Pizzas have been ordered.");
         System.out.println(totalGrossRevenue+" $ Of gross revenue has been made since opening today.");
-        System.out.println("We have "+totalMeat+" ounces of meat left.");
-        System.out.println("We have "+totalBuns+" buns left.");
-        
         System.out.println("*****END OF STATISTICS*****");        
     }
-    
+    public static void product()
+    {
+        System.out.println("We have "+totalMeat+" ounces of meat");
+        System.out.println("We have "+totalBuns+" buns");
+        System.out.println("We have "+pastaServings+" servings of pasta");
+        System.out.println("We have "+pastaSauce+" cups of sauce");
+        System.out.println("We have "+cheese+" pounds of cheese");
+        System.out.println("We have "+dough+" pounds of dough");
+    }
 }
